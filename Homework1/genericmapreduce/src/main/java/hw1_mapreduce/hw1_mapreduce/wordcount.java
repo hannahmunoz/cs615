afe9f623f16e89427a8c3694a01f05943b889ee8
Hannah Munoz <hannah.munoz92@gmail.com>
2018-03-08 13:58:26 -0800

    can count hashtags now

--- Homework1/genericmapreduce/src/main/java/hw1_mapreduce/hw1_mapreduce/wordcount.java
+++ Homework1/genericmapreduce/src/main/java/hw1_mapreduce/hw1_mapreduce/wordcount.java
@@ -5,6 +5,7 @@ import java.util.StringTokenizer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.IntWritable.Comparator;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -19,7 +20,7 @@ public class wordcount {
 	// so it more closely resembles his code
 	
 	// basic mapper function
-	public static class mapper extends Mapper<Object, Text, Text, IntWritable>{
+	public static class tweetMapper extends Mapper<Object, Text, Text, IntWritable>{
 		public void map (Object key, Text value, Context context) throws IOException, InterruptedException {
 			// current word
 			StringTokenizer st = new StringTokenizer(value.toString());
@@ -29,26 +30,37 @@ public class wordcount {
 			
 			// loop through file until all words have been counted and found
 			while (st.hasMoreTokens()) {
-				wordOut.set(st.nextToken());
-				context.write(wordOut, one);
+				String token = st.nextToken();
+				//locate hashtag
+				if (token.startsWith("#")) {
+					//split in case of mutliple tags
+					String[] tokenList = token.split("#");
+					for (String i : tokenList) {
+						wordOut.set(i);
+						context.write(wordOut, one);
+					}
+				}
 			}
 		}
 	}
 	
 	// basic reducer function
-	public static class reducer extends Reducer <Text, IntWritable, Text, IntWritable>{
+	public static class hashReducer extends Reducer <Text, IntWritable,  IntWritable, Text>{
 		public void reduce(Text term, Iterable<IntWritable> value, Context context) throws IOException, InterruptedException  {
-			int count = 0;
-			// count all the occurances of the tweet
-			while (value.iterator().hasNext()) {
-				count++;
-				value.iterator().next();
+			//top 100
+			//for (int i = 0; i < 100; i++) {
+				int count = 0;
+				// count all the occurances of the tweet
+				while (value.iterator().hasNext()) {
+					count++;
+					value.iterator().next();
+				}
+				
+				// write it to a file
+				IntWritable output = new  IntWritable (count);
+				context.write(output, term);
 			}
-			
-			// write it to a file
-			IntWritable output = new  IntWritable (count);
-			context.write(term, output);
-		}
+			//}
 	}
 	
 	
@@ -65,13 +77,18 @@ public class wordcount {
 		// set up tasks
 		Job job = Job.getInstance(conf, "Word Count");
 		job.setJarByClass(wordcount.class);
-		job.setMapperClass(mapper.class);
-		job.setReducerClass(reducer.class);
+		//gets and reduces all hashtag
+		job.setMapperClass(tweetMapper.class);
+		job.setReducerClass(hashReducer.class);
 		
 		// 3 taks per hw requirements
-		job.setNumReduceTasks(3);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
+		job.setNumReduceTasks(1);
+		job.setMapOutputKeyClass(Text.class);
+	    job.setMapOutputValueClass(IntWritable.class);
+	    
+	    
+		job.setOutputKeyClass(IntWritable.class);
+		job.setOutputValueClass(Text.class);
 		
 		// set up input and output files
 		FileInputFormat.addInputPath(job, new Path(argurments[0]));
